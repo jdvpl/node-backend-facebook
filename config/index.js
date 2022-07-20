230e7bd1f17b4d837a6373caca6224a045fb67a0
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-07-20 10:24:56 -0500

    creating the user model

--- config/index.js
+++ config/index.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const dbConection = async () => {
   try {
     // opciones {autoIndex:false,}
-    await mongoose.connect(process.env.MONGODBCOONECTION);
+    await mongoose.connect(process.env.MONGODBCOONECTION, {
+      useNewUrlParser: true,
+    });
     console.log("Db conected");
   } catch (error) {
     throw new Error(`Database failed`);
--- models/user.js
+++ models/user.js
@@ -1,44 +1,147 @@
-const {Schema,model}= require('mongoose');
+const { Schema, model } = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const userSchema = Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      text: true,
+    },
+    last_name: {
+      type: String,
+      required: [true, "Last name is required"],
+      trim: true,
+      text: true,
+    },
+    user_name: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      text: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    pricture: {
+      type: String,
+      trim: true,
+      default:
+        "https://res.cloudinary.com/jdvpl/image/upload/v1656359029/man-g761407816_1280_r08dyd.png",
+    },
+    cover: {
+      type: String,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: [true, "Gender is required"],
+      trim: true,
+    },
+    bYear: {
+      type: Number,
+      required: [true, "The birth year is required"],
+      trim: true,
+    },
+    bMonth: {
+      type: Number,
+      required: [true, "The birth month is required"],
+      trim: true,
+    },
+    bDay: {
+      type: Number,
+      required: [true, "The birth day is required"],
+      trim: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    requests: {
+      type: Array,
+      default: [],
+    },
+    search: [
+      {
+        type: ObjectId,
+        ref: "User",
+      },
+    ],
+    details: {
+      bio: {
+        type: String,
+      },
+      otherName: {
+        type: String,
+      },
+      job: {
+        type: String,
+      },
+      workplace: {
+        type: String,
+      },
+      highSchool: {
+        type: String,
+      },
+      college: {
+        type: String,
+      },
+      currentCity: {
+        type: String,
+      },
+      hometown: {
+        type: String,
+      },
+      relationship: {
+        type: String,
+        enum: ["Single", "In a relationship", "Married", "Divorced"],
+      },
+      instagram: {
+        type: String,
+      },
+    },
+    savedPosts: [
+      {
+        post: {
+          type: ObjectId,
+          ref: "Post",
+        },
+        savedAt: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
+userSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...user } = this.toObject();
+  user.uid = _id;
+  return user;
+};
 
-const UsuarioSchema=Schema({
-  name:{
-    type:String,
-    required:[true,'El nombre es obligatorio'],
-  },
-  email:{
-    type:String,
-    required:[true,'El correo es obligatorio'],
-    unique:true
-  },
-  password:{
-    type:String,
-    required:[true,'El contraseña es obligatoria'],
-  },
-  img:{
-    type:String,
-  },
-  role:{
-    type:String,
-    default:'USER_ROLE',
-    required:[true,'El rol es obligatorio'],
-    // enum:['ADMIN_ROLE','USER_ROLE']
-  },
-  status:{
-    type:Boolean,
-    default:true
-  },
-  google:{
-    type:Boolean,
-    default:false
-  },
-})
-
-
-UsuarioSchema.methods.toJSON=function(){
-  const {__v,password,_id,...user}=this.toObject();
-  user.uid=_id;
-  return user
-}
-
-module.exports=model('User',UsuarioSchema);
+module.exports = model("User", userSchema);
