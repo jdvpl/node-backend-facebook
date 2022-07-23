f8cfb58ed9a27240f8ba39d54b06748dc3e99263
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-07-23 16:48:59 -0500

    username validation bug solved

--- helpers/validation.js
+++ helpers/validation.js
@@ -1,17 +1,17 @@
 const User = require("../models/user");
 
-const validateUsername = async (user_name = "") => {
+const validateUsername = async (username = "") => {
   let a = false;
   do {
-    const existsUser = await User.findOne({ user_name });
+    const existsUser = await User.findOne({ username });
     if (existsUser) {
-      user_name += (+new Date() * Math.random()).toString().substring(0, 1);
+      username += (+new Date() * Math.random()).toString().substring(0, 1);
       a = true;
     } else {
       a = false;
     }
   } while (a);
-  return user_name;
+  return username;
 };
 
 module.exports = { validateUsername };
